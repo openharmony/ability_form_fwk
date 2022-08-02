f3e97960fda6b4a57fd33578f1337b3da9fbd467
xuchenghua09 <xuchenghua09@huawei.com>
2022-08-02 10:40:22 +0800

    form share free install
    Signed-off-by: xuchenghua09 <xuchenghua09@huawei.com>

---
+++ services/include/free_install_status_callback_proxy.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FOUNDATION_ABILITY_FORM_FWK_SERVICES_INCLUDE_FREE_INSTALL_STATUS_CALLBACK_PROXY_H
+#define FOUNDATION_ABILITY_FORM_FWK_SERVICES_INCLUDE_FREE_INSTALL_STATUS_CALLBACK_PROXY_H
+
+#include "iremote_proxy.h"
+#include "i_free_install_status_callback.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+/**
+ * @class FreeInstallStatusCallBackProxy
+ * FreeInstallStatusCallBackProxy.
+ */
+class FreeInstallStatusCallBackProxy : public IRemoteProxy<IFreeInstallStatusCallBack> {
+public:
+    explicit FreeInstallStatusCallBackProxy(const sptr<IRemoteObject>& impl);
+
+    /**
+     * OnInstallFinished, FreeInstall is complete.
+     *
+     * @param resultCode, ERR_OK on success, others on failure.
+     * @param want, installed ability.
+     * @param userId, user`s id.
+     */
+    void OnInstallFinished(int32_t resultCode, const Want &want, int32_t userId) override;
+
+private:
+    void SendRequestCommon(int32_t errcode, IFreeInstallStatusCallBack::IFreeInstallStatusCallBackCmd cmd);
+    static inline BrokerDelegator<FreeInstallStatusCallBackProxy> delegator_;
+};
+} // namespace AppExecFwk
+} // namespace OHOS
+#endif  // FOUNDATION_ABILITY_FORM_FWK_SERVICES_INCLUDE_FREE_INSTALL_STATUS_CALLBACK_PROXY_H
---
+++ services/src/form_free_install_operator.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "form_free_install_operator.h"
+#include "form_bms_helper.h"
+#include "form_mgr_errors.h"
+#include "form_share_mgr.h"
+#include "form_util.h"
+#include "hilog_wrapper.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+FormFreeInstallOperator::FormFreeInstallOperator(const std::string &formShareInfoKey,
+    const std::shared_ptr<AppExecFwk::EventHandler> &handler)
+    : formShareInfoKey_(formShareInfoKey), handler_(handler)
+{
+}
+
+FormFreeInstallOperator::~FormFreeInstallOperator()
+{
+    freeInstallStatusCallBack_ = nullptr;
+}
+
+int32_t FormFreeInstallOperator::StartFreeInstall(
+    const std::string &bundleName, const std::string &moduleName, const std::string &abilityName)
+{
+    HILOG_DEBUG("%{public}s called, bundleName: %{public}s, abilityName: %{public}s",
+        __func__, bundleName.c_str(), abilityName.c_str());
+
+    freeInstallStatusCallBack_ = new (std::nothrow) FreeInstallStatusCallBack(weak_from_this());
+    if (freeInstallStatusCallBack_ == nullptr) {
+        HILOG_ERROR("new FreeInstallStatusCallBack failed");
+        return ERR_NO_MEMORY;
+    }
+
+    sptr<IBundleMgr> iBundleMgr = FormBmsHelper::GetInstance().GetBundleMgr();
+    if (iBundleMgr == nullptr) {
+        HILOG_ERROR("get IBundleMgr failed");
+        return ERR_NO_MEMORY;
+    }
+
+    Want want;
+    want.SetElementName(bundleName, abilityName);
+    want.SetModuleName(moduleName);
+    AppExecFwk::AbilityInfo abilityInfo = {};
+    constexpr auto flag = AppExecFwk::AbilityInfoFlag::GET_ABILITY_INFO_WITH_APPLICATION;
+    if (iBundleMgr->QueryAbilityInfo(
+        want, flag, FormUtil::GetCurrentAccountId(), abilityInfo, freeInstallStatusCallBack_)) {
+        HILOG_DEBUG("The app has installed.");
+    }
+
+    return ERR_OK;
+}
+
+void FormFreeInstallOperator::OnInstallFinished(int32_t resultCode)
+{
+    HILOG_DEBUG("%{public}s called, resultCode: %{public}d", __func__, resultCode);
+
+    auto self = shared_from_this();
+    auto task = [self, resultCode]() {
+        FormShareMgr::GetInstance().OnInstallFinished(self, resultCode, self->formShareInfoKey_);
+    };
+    handler_->PostTask(task);
+}
+
+FreeInstallStatusCallBack::FreeInstallStatusCallBack(
+    const std::weak_ptr<FormFreeInstallOperator> &freeInstallOperator)
+    : formFreeInstallOperator_(freeInstallOperator)
+{
+}
+
+void FreeInstallStatusCallBack::OnInstallFinished(int32_t resultCode, const Want &want, int32_t userId)
+{
+    HILOG_DEBUG("%{public}s called, resultCode:%{public}d.", __func__, resultCode);
+
+    auto freeInstallOperator = formFreeInstallOperator_.lock();
+    if (freeInstallOperator == nullptr) {
+        HILOG_ERROR("freeInstallOperator is nullptr");
+        return;
+    }
+    freeInstallOperator->OnInstallFinished(resultCode);
+    HILOG_DEBUG("%{public}s end.", __func__);
+}
+} // namespace AppExecFwk
+} // namespace OHOS
