3fe659c081c2eb439fcff16b83ed38986fda1c29
liuyanzhi <liuyanzhi3@huawei.com>
2022-11-03 15:48:49 +0800

    fixed 8ae61b4 from https://gitee.com/liuyanzhi2020/ability_form_fwk/pulls/278 add fork fuzz test
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I6d6967b7fa178e37e1a892eb5dee043d47d2eabe

---
+++ test/fuzztest/formproviderdata_fuzzer/formproviderdata_fuzzer.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "formproviderdata_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#define private public
+#define protected public
+#include "form_provider_data.h"
+#undef private
+#undef protected
+#include "securec.h"
+
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+constexpr size_t FOO_MAX_LEN = 1024;
+uint32_t GetU32Data(const char* ptr)
+{
+    // convert fuzz input data to an integer
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+}
+bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+{
+    FormProviderData formProviderData;
+    nlohmann::json jsonData;
+    formProviderData.UpdateData(jsonData);
+    formProviderData.GetData();
+    std::string picName(data, size);
+    const std::shared_ptr<char> datas = nullptr;
+    int32_t sizes = static_cast<int32_t>(GetU32Data(data));
+    formProviderData.AddImageData(picName, datas, sizes);
+    int fd = static_cast<int>(GetU32Data(data));
+    formProviderData.AddImageData(picName, fd);
+    formProviderData.ParseImagesData();
+    formProviderData.RemoveImageData(picName);
+    std::string jsonDataString(data, size);
+    formProviderData.SetDataString(jsonDataString);
+    nlohmann::json addJsonData;
+    formProviderData.MergeData(addJsonData);
+    formProviderData.GetImageDataMap();
+    int32_t imageDataState = static_cast<int32_t>(GetU32Data(data));
+    formProviderData.SetImageDataState(imageDataState);
+    Parcel parcel;
+    formProviderData.ReadFromParcel(parcel);
+    formProviderData.Marshalling(parcel);
+    formProviderData.Unmarshalling(parcel);
+    formProviderData.ClearData();
+    formProviderData.NeedCache();
+    formProviderData.WriteImageDataToParcel(parcel, picName, datas, sizes);
+    formProviderData.ConvertRawImageData();
+    return formProviderData.GetDataString() == "aa";
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size == 0 || size > OHOS::FOO_MAX_LEN) {
+        return 0;
+    }
+
+    char* ch = (char *)malloc(size + 1);
+    if (ch == nullptr) {
+        return 0;
+    }
+
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size, data, size) != EOK) {
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+    free(ch);
+    ch = nullptr;
+    return 0;
+}
+
