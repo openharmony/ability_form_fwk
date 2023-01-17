d099d0c44a2445b85a7d4d3e550933d72b8f8f1f
xinking129 <xinxin13@huawei.com>
2023-01-17 11:28:11 +0800

    add fms_form_mgr_service_test
    Signed-off-by: xinking129 <xinxin13@huawei.com>

---
+++ test/unittest/fms_form_mgr_service_test/mock_form_share_mgr.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License")_;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "form_share_mgr.h"
+#include "form_mgr_errors.h"
+
+namespace {
+    int32_t shareForm = OHOS::ERR_OK;
+}
+
+void MockShareForm(bool mockRet)
+{
+    shareForm = mockRet;
+}
+
+namespace OHOS {
+namespace AppExecFwk {
+int32_t FormShareMgr::ShareForm(
+    int64_t formId, const std::string &deviceId, const sptr<IRemoteObject> &callerToken, int64_t requestCode)
+{
+    GTEST_LOG_(INFO) << "ShareForm called " << shareForm;
+    return shareForm;
+}
+}  // namespace AppExecFwk
+}  // namespace OHOS
