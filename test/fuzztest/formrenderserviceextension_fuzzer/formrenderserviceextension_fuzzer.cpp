75159611ea88e046d75a14d9a51bd5e0c2bc620d
liuyanzhi <liuyanzhi3@huawei.com>
2023-03-09 07:28:08 +0000

    fixed ad9c639 from https://gitee.com/liuyanzhi2020/ability_form_fwk/pulls/602 add render fuzz
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I555e1b115392010be918a70faa849b1458fa500d

---
+++ test/fuzztest/formrenderserviceextension_fuzzer/formrenderserviceextension_fuzzer.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "formrenderserviceextension_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#define private public
+#define protected public
+#include "form_render_service_extension.h"
+#undef private
+#undef protected
+#include "message_parcel.h"
+#include "securec.h"
+
+using namespace OHOS::AppExecFwk;
+using namespace OHOS::AbilityRuntime;
+
+namespace OHOS {
+constexpr size_t FOO_MAX_LEN = 1024;
+constexpr size_t U32_AT_SIZE = 4;
+constexpr uint8_t ENABLE = 2;
+uint32_t GetU32Data(const char* ptr)
+{
+    // convert fuzz input data to an integer
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+}
+bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+{
+    std::unique_ptr<Runtime> runtime = nullptr;
+    auto formRenderServiceExtension = FormRenderServiceExtension::Create(runtime);
+    if (formRenderServiceExtension == nullptr) {
+        return false;
+    }
+    std::shared_ptr<AbilityLocalRecord> record = nullptr;
+    std::shared_ptr<OHOSApplication> application = nullptr;
+    std::shared_ptr<AbilityHandler> handler = nullptr;
+    sptr<IRemoteObject> token = nullptr;
+    formRenderServiceExtension->Init(record, application, handler, token);
+    AAFwk::Want want;
+    bool restart = *data % ENABLE;
+    int startId = static_cast<int64_t>(GetU32Data(data));
+    formRenderServiceExtension->OnCommand(want, restart, startId);
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    if (size < OHOS::U32_AT_SIZE) {
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size == 0 || size > OHOS::FOO_MAX_LEN) {
+        return 0;
+    }
+
+    char* ch = (char *)malloc(size + 1);
+    if (ch == nullptr) {
+        return 0;
+    }
+
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size, data, size) != EOK) {
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+    free(ch);
+    ch = nullptr;
+    return 0;
+}
+
