a3d2cbedb751172a685e92104639f037a5daabfc
dujingcheng <dujingcheng@huawei.com>
2023-07-03 01:15:02 +0000

    fuzz
    Signed-off-by: dujingcheng <dujingcheng@huawei.com>
    Change-Id: I28390e1d60f775e3eb63bf5d59f5bfe56ab0f331

--- test/fuzztest/formrenderimpl_fuzzer/formrenderimpl_fuzzer.cpp
+++ test/fuzztest/formrenderimpl_fuzzer/formrenderimpl_fuzzer.cpp
@@ -48,9 +48,10 @@ bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
     sptr<IRemoteObject> hostToken = nullptr;
     formRenderImpl.CleanFormHost(hostToken);
     int64_t formId = static_cast<int64_t>(GetU32Data(data));
-    std::vector<int64_t> formIds;
-    formIds.emplace_back(formId);
-    formRenderImpl.ReloadForm(std::move(formIds), want);
+    std::vector<FormJsInfo> formJsInfos;
+    formJsInfo.formId = formId;
+    formJsInfos.emplace_back(formJsInfo);
+    formRenderImpl.ReloadForm(std::move(formJsInfos), want);
     return true;
 }
 }
