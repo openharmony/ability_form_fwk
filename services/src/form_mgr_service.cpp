707822c655614520caf00fac2a9398d4a2a4bdaf
wangkailong <wangkailong6@huawei.com>
2024-01-12 17:03:59 +0800

    switchuser
    Signed-off-by: wangkailong <wangkailong6@huawei.com>
    Change-Id: I5dfa551cc1e5ee1c8ea051203b148b6f91492af0

--- services/src/form_mgr_service.cpp
+++ services/src/form_mgr_service.cpp
@@ -640,6 +640,7 @@ void FormMgrService::SubscribeSysEventReceiver()
         matchingSkills.AddEvent(EventFwk::CommonEventSupport::COMMON_EVENT_USER_REMOVED);
         matchingSkills.AddEvent(EventFwk::CommonEventSupport::COMMON_EVENT_USER_UNLOCKED);
         matchingSkills.AddEvent(EventFwk::CommonEventSupport::COMMON_EVENT_BUNDLE_SCAN_FINISHED);
+        matchingSkills.AddEvent(EventFwk::CommonEventSupport::COMMON_EVENT_USER_SWITCHED);
         // init TimerReceiver
         EventFwk::CommonEventSubscribeInfo subscribeInfo(matchingSkills);
         subscribeInfo.SetThreadMode(EventFwk::CommonEventSubscribeInfo::COMMON);
