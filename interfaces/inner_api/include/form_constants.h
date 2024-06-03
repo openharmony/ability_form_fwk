3bd2cf754c6895fd0014a4a1444d5e1e90da6b18
s00800643 <shuxinyue1@huawei.com>
2024-06-03 17:40:19 +0800

    修复已回收卡片识别点击失败，添加常量
    Signed-off-by: s00800643 <shuxinyue1@huawei.com>
    Change-Id: Ia6be2912ccdb11b779a58e8e36fa41ce85d4f286

--- interfaces/inner_api/include/form_constants.h
+++ interfaces/inner_api/include/form_constants.h
@@ -160,6 +160,8 @@ namespace Constants {
     constexpr const char* FORM_RENDER_COMP_ID = "form_render_comp_id";
     constexpr const char* FORM_DELAY_TIME_OF_RECYCLE = "form_delay_time_of_recycle";
     constexpr const char* FORM_STATUS_DATA = "form_status_data";
+    constexpr const char* FORM_IS_DYNAMIC = "form_is_dynamic";
+    constexpr const char* FORM_IS_RECOVER_FORM = "form_is_recover_form";
     constexpr const char* FORM_IS_RECOVER_FORM_TO_HANDLE_CLICK_EVENT = "form_is_recover_form_to_handle_click_event";
 
     const size_t MAX_LAYOUT = 8;
