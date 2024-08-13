80193614dae417dc48f82f475cbcad217bf3b974
jiangzhijun8 <jiangzhijun7@huawei.com>
2024-08-13 00:55:21 -0700

    fix: 日历白卡异常
    Signed-off-by: jiangzhijun8 <jiangzhijun7@huawei.com>

--- interfaces/inner_api/include/form_constants.h
+++ interfaces/inner_api/include/form_constants.h
@@ -93,6 +93,7 @@ namespace Constants {
     constexpr const char* PARAM_PUBLISH_FORM_HOST_SCREENY_KEY = "ohos.extra.param.key.add_form_to_host_screeny";
     constexpr const char* CPU_SCENE_ID_CONFIG_UPDATE  = "1";
     constexpr const char* RECYCLE_FORMS_USER_ID = "ohos.extra.param.key.recycle_forms_user_id";
+    constexpr const char* PARAM_DYNAMIC_NAME_KEY = "isDynamic";
 
     constexpr int32_t UNKNOWN = 0;
     // The form events type which means that the form becomes visible.
