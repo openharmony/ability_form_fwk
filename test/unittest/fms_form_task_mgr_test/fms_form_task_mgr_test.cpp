e4f6f7fafdba131ba7afec4c4fee8033d84aa14b
jiangzhijun8 <jiangzhijun7@huawei.com>
2024-11-09 10:20:21 +0800

    补充FmsFormCacheMgrTest和FmsFormDataProxyMgrTest测试用例
    Signed-off-by: jiangzhijun8 <jiangzhijun7@huawei.com>

--- test/unittest/fms_form_task_mgr_test/fms_form_task_mgr_test.cpp
+++ test/unittest/fms_form_task_mgr_test/fms_form_task_mgr_test.cpp
@@ -21,6 +21,10 @@
 #include "fms_log_wrapper.h"
 #include "mock_form_provider_client.h"
 #include "gmock/gmock.h"
+#include "form_render_interface.h"
+#include "form_share_mgr.h"
+#include "js_form_state_observer_interface.h"
+#include "form_host_interface.h"
 
 using namespace testing::ext;
 using namespace OHOS;
@@ -888,5 +892,430 @@ HWTEST_F(FormTaskMgrTest, FormTaskMgr_0050, TestSize.Level0)
     sptr<IRemoteObject> remoteObject = new (std::nothrow) MockFormProviderClient();
     formTaskMgr.AcquireFormDataBack(wantParams, requestCode, remoteObject);
     GTEST_LOG_(INFO) << "FormTaskMgr_0050 end";
+}
+
+/**
+ * @tc.name: FormTaskMgr_0051
+ * @tc.desc: test PostRenderForm function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0051, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0051 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    FormRecord record;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    const std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostRenderForm(record, want, remoteObject);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0051 end";
+}
+
+/**
+ * @tc.name: FormTaskMgr_0052
+ * @tc.desc: test PostStopRenderingForm function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0052, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0052 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    FormRecord record;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->PostStopRenderingForm(record, want, remoteObject);
+    int64_t formId = 0;
+    std::string compId = "compId";
+    std::string uid = "uid";
+    formTaskMgr->PostReleaseRenderer(formId, compId, uid, remoteObject);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostStopRenderingForm(record, want, remoteObject);
+    formTaskMgr->PostReleaseRenderer(formId, compId, uid, remoteObject);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0052 end";
+}
+
+/**
+ * @tc.name: FormTaskMgr_0053
+ * @tc.desc: test ReleaseRenderer function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0053, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0053 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    int64_t formId = 0;
+    std::string compId = "compId";
+    std::string uid = "uid";
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->ReleaseRenderer(formId, compId, uid, remoteObject);
+    FormRecord record;
+    Want want;
+    formTaskMgr->RenderForm(record, want, remoteObject);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0053 end";
+}
+
+/**
+ * @tc.name: FormTaskMgr_0054
+ * @tc.desc: test ReloadForm function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0054, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0054 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    std::vector<FormRecord> formRecords;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->ReloadForm(std::move(formRecords), want, remoteObject);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0054 end";
+}
+
+/**
+ * @tc.name: FormTaskMgr_0054
+ * @tc.desc: test FormShareSendResponse function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0055, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0055 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    int64_t formShareRequestCode = 0;
+    int32_t result = 0;
+    formTaskMgr->FormShareSendResponse(formShareRequestCode, result);
+    EXPECT_NE(DelayedSingleton<FormShareMgr>::GetInstance(), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0055 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: StopRenderingForm
+ * FunctionPoints: FormCacheMgr StopRenderingForm interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0056, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0056 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    FormRecord formRecords;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->StopRenderingForm(formRecords, want, remoteObject);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0056 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostReloadForm
+ * FunctionPoints: FormCacheMgr PostReloadForm interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0057, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0057 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    std::vector<FormRecord> formRecords;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->PostReloadForm(std::move(formRecords), want, remoteObject);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostReloadForm(std::move(formRecords), want, remoteObject);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0057 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: OnUnlock
+ * FunctionPoints: FormCacheMgr OnUnlock interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0058, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0058 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->OnUnlock(remoteObject);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0058 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostOnUnlock
+ * FunctionPoints: FormCacheMgr PostOnUnlock interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0059, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0059 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->PostOnUnlock(remoteObject);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostOnUnlock(remoteObject);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0059 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: FormRouterEventProxy
+ * FunctionPoints: FormCacheMgr FormRouterEventProxy interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0060, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0060 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    int64_t formId = 0;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->FormRouterEventProxy(formId, remoteObject, want);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    remoteObject = new (std::nothrow) MockFormProviderClient();
+    formTaskMgr->PostOnUnlock(remoteObject);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0060 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostRecycleForms
+ * FunctionPoints: FormCacheMgr PostRecycleForms interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0061, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0061 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    std::vector<int64_t> formIds;
+    Want want;
+    sptr<IRemoteObject> remoteObjectOfHost = nullptr;
+    sptr<IRemoteObject> remoteObjectOfRender = nullptr;
+    formTaskMgr->PostRecycleForms(formIds, want, remoteObjectOfHost, remoteObjectOfRender);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostRecycleForms(formIds, want, remoteObjectOfHost, remoteObjectOfRender);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0061 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostRecycleForms
+ * FunctionPoints: FormCacheMgr PostRecycleForms interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0062, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0062 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    int64_t formId = 1;
+    sptr<IRemoteObject> remoteObjectOfHost = nullptr;
+    sptr<IRemoteObject> remoteObjectOfRender = nullptr;
+    formTaskMgr->RecycleForm(formId, remoteObjectOfHost, remoteObjectOfRender);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObjectOfRender), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0062 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostRecoverForm
+ * FunctionPoints: FormCacheMgr PostRecoverForm interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0063, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0063 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    FormRecord record;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->PostRecoverForm(record, want, remoteObject);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostRecoverForm(record, want, remoteObject);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0063 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: RecoverForm
+ * FunctionPoints: FormCacheMgr PostRecycleForms interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0064, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0064 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    FormRecord record;
+    Want want;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->RecoverForm(record, want, remoteObject);
+    EXPECT_EQ(iface_cast<IFormRender>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0064 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: CancelDelayTask
+ * FunctionPoints: FormCacheMgr CancelDelayTask interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0065, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0065 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    std::pair<int64_t, int64_t> eventMsg;
+    formTaskMgr->CancelDelayTask(eventMsg);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->CancelDelayTask(eventMsg);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0065 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostFormClickEventToHost
+ * FunctionPoints: FormCacheMgr PostFormClickEventToHost interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0066, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0066 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    std::string bundleName = "bundleName";
+    std::string formEventType = "formEventType";
+    sptr<IRemoteObject> remoteObject = nullptr;
+    RunningFormInfo runningFormInfo;
+    formTaskMgr->PostFormClickEventToHost(bundleName, formEventType, remoteObject, runningFormInfo);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostFormClickEventToHost(bundleName, formEventType, remoteObject, runningFormInfo);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0066 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: FormClickEvent
+ * FunctionPoints: FormCacheMgr FormClickEvent interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0067, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0067 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    std::string bundleName = "bundleName";
+    std::string formEventType = "formEventType";
+    sptr<IRemoteObject> remoteObject = nullptr;
+    RunningFormInfo runningFormInfo;
+    formTaskMgr->FormClickEvent(bundleName, formEventType, remoteObject, runningFormInfo);
+    EXPECT_EQ(iface_cast<AbilityRuntime::IJsFormStateObserver>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0067 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostBatchRefreshForms
+ * FunctionPoints: FormCacheMgr PostBatchRefreshForms interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0068, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0068 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    int32_t formRefreshType = 1;
+    formTaskMgr->PostBatchRefreshForms(formRefreshType);
+    EXPECT_EQ(formTaskMgr->serialQueue_, nullptr);
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostBatchRefreshForms(formRefreshType);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0068 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: EnableFormsTaskToHost
+ * FunctionPoints: FormCacheMgr EnableFormsTaskToHost interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0069, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0069 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    vector<int64_t> formIds;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    formTaskMgr->EnableFormsTaskToHost(formIds, false, remoteObject);
+    EXPECT_EQ(iface_cast<IFormHost>(remoteObject), nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0069 end";
+}
+
+/*
+ * Feature: FormCacheMgr
+ * Function: PostTask
+ * FunctionPoints: FormCacheMgr PostTask interface
+ * EnvConditions: Mobile that can run ohos test framework
+ */
+HWTEST_F(FormTaskMgrTest, FormTaskMgr_0070, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "FormTaskMgr_0070 start";
+    std::shared_ptr<FormTaskMgr> formTaskMgr = std::make_shared<FormTaskMgr>();
+    ASSERT_NE(formTaskMgr, nullptr);
+    uint64_t delayMs = 1;
+    std::function<void()> func = nullptr;
+    formTaskMgr->PostTask(func, delayMs);
+    vector<int64_t> formIds;
+    sptr<IRemoteObject> remoteObject = nullptr;
+    bool enable = false;
+    auto enableFormsTaskToHostFunc = [formIds, enable, remoteObject]()
+    {
+        FormTaskMgr::GetInstance().EnableFormsTaskToHost(formIds, enable, remoteObject);
+    };
+    std::string queueName = "queue";
+    formTaskMgr->serialQueue_ = std::make_shared<FormSerialQueue>(queueName.c_str());
+    formTaskMgr->PostFrsDiedTaskToHost(remoteObject);
+    formTaskMgr->PostTask(enableFormsTaskToHostFunc, delayMs);
+    EXPECT_NE(formTaskMgr->serialQueue_, nullptr);
+    GTEST_LOG_(INFO) << "FormTaskMgr_0070 end";
 }
 }
