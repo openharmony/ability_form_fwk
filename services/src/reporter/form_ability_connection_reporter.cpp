8ab49d9768c9f43197563054e1b7712eec4c6f42
houzhixiong <houzhixiong1@h-partners.com>
2025-02-26 19:16:55 +0800

    connections链接
    Signed-off-by: houzhixiong <houzhixiong1@h-partners.com>

---
+++ services/src/reporter/form_ability_connection_reporter.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unordered_map>
+
+#include "form_ability_connection_reporter.h"
+#include "fms_log_wrapper.h"
+#include "form_mgr_errors.h"
+#include "form_util.h"
+#include "iremote_proxy.h"
+#include "iservice_registry.h"
+#include "system_ability_definition.h"
+#ifdef RES_SCHEDULE_ENABLE
+#include "res_sched_client.h"
+#include "res_type.h"
+#endif
+
+namespace OHOS {
+namespace AppExecFwk {
+namespace {
+const std::string FORM_CONNECTION_PROCESS_NAME_SUFFIX = ":form";
+}
+
+FormAbilityConnectionReporter::FormAbilityConnectionReporter()
+{
+    HILOG_INFO("Call");
+}
+
+FormAbilityConnectionReporter::~FormAbilityConnectionReporter()
+{
+    HILOG_INFO("Call");
+}
+
+void FormAbilityConnectionReporter::ReportFormAbilityConnection(const std::string &bundleName)
+{
+    if (bundleName.empty()) {
+        HILOG_ERROR("Invalid bundleName");
+        return;
+    }
+    std::lock_guard<std::mutex> lock(formConnectionInfoMapMutex_);
+    auto iter = formConnectionInfoMap_.find(bundleName);
+    if (iter != formConnectionInfoMap_.end()) {
+        FormConnectionInfo &connectionInfo = iter->second;
+        connectionInfo.connectCount_++;
+        return;
+    }
+    auto appMgr = GetAppMgr();
+    std::vector<AppExecFwk::RunningProcessInfo> infos;
+    int32_t userId = FormUtil::GetCurrentAccountId();
+    int32_t ret = appMgr->GetRunningProcessInformation(bundleName, userId, infos);
+    if (ret != ERR_OK) {
+        HILOG_ERROR("Get running process info failed");
+        return;
+    }
+    HILOG_INFO("BundleName:%{public}s, infosSize:%{public}zu",
+        bundleName.c_str(), infos.size());
+    AddFormAbilityConnectProcessInfo(bundleName, infos);
+    ReportConnectionInfosToRss(bundleName, true);
+}
+
+void FormAbilityConnectionReporter::ReportFormAbilityDisconnection(const std::string &bundleName)
+{
+    if (bundleName.empty()) {
+        HILOG_ERROR("Invalid bundleName");
+        return;
+    }
+    std::lock_guard<std::mutex> lock(formConnectionInfoMapMutex_);
+    auto iter = formConnectionInfoMap_.find(bundleName);
+    if (iter == formConnectionInfoMap_.end()) {
+        HILOG_ERROR("Disconnect ability connection:%{public}s, but without created connection notified!",
+            bundleName.c_str());
+        return;
+    }
+    int count = iter->second.connectCount_ - 1;
+    if (count > 0) {
+        formConnectionInfoMap_[bundleName].connectCount_ = count;
+        return;
+    }
+
+    ReportConnectionInfosToRss(bundleName, false);
+    formConnectionInfoMap_.erase(bundleName);
+    HILOG_INFO("bundleName:%{public}s", bundleName.c_str());
+}
+
+sptr<OHOS::AppExecFwk::IAppMgr> FormAbilityConnectionReporter::GetAppMgr()
+{
+    if (appManager_ == nullptr) {
+        sptr<ISystemAbilityManager> systemMgr = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+        if (systemMgr == nullptr) {
+            HILOG_ERROR("connect systemAbilityManager failed");
+            return nullptr;
+        }
+        sptr<IRemoteObject> remoteObject = systemMgr->GetSystemAbility(APP_MGR_SERVICE_ID);
+        if (remoteObject == nullptr) {
+            HILOG_ERROR("connect appMgrService failed");
+            return nullptr;
+        }
+        HILOG_INFO("ConnectAMS succeed");
+        appManager_ = iface_cast<OHOS::AppExecFwk::IAppMgr>(remoteObject);
+    }
+    return appManager_;
+}
+
+void FormAbilityConnectionReporter::AddFormAbilityConnectProcessInfo(const std::string& bundleName,
+    std::vector<AppExecFwk::RunningProcessInfo>& infos)
+{
+    if (bundleName.empty()) {
+        HILOG_WARN("Empty bundle name");
+        infos.clear();
+        return;
+    }
+    std::string targetProceName = bundleName + FORM_CONNECTION_PROCESS_NAME_SUFFIX;
+    for (std::vector<AppExecFwk::RunningProcessInfo>::iterator iter = infos.begin(); iter != infos.end(); iter++) {
+        if ((*iter).processName_ != targetProceName) {
+            continue;
+        }
+        HILOG_INFO("Get running process:%{public}s, pid:%{public}" PRId32, targetProceName.c_str(),
+            (*iter).pid_);
+        FormConnectionInfo connectionInfo;
+        connectionInfo.pid_ = (*iter).pid_;
+        connectionInfo.connectCount_ = 1;
+        formConnectionInfoMap_.emplace(bundleName, connectionInfo);
+        return;
+    }
+}
+
+void FormAbilityConnectionReporter::ReportConnectionInfosToRss(const std::string &bundleName,
+    bool isConnected)
+{
+    auto iter = formConnectionInfoMap_.find(bundleName);
+    if (iter == formConnectionInfoMap_.end()) {
+        HILOG_WARN("Report connection info failed, empty info, bundle name:%{public}s",
+            bundleName.c_str());
+        return;
+    }
+#ifdef RES_SCHEDULE_ENABLE
+    auto status = isConnected ? ResourceSchedule::ResType::FormCreateStatus::FormCreateStart
+        : ResourceSchedule::ResType::FormCreateStatus::FormCreateEnd;
+    std::unordered_map<std::string, std::string> payload = {
+        {"saId", std::to_string(FORM_MGR_SERVICE_ID)},
+        {"pid", std::to_string(iter->second.pid_)}
+    };
+    ResourceSchedule::ResSchedClient::GetInstance().ReportData(
+        ResourceSchedule::ResType::RES_TYPE_FORM_STATE_CHANGE_EVENT, status, payload);
+#endif
+}
+}  // namespace AppExecFwk
+}  // namespace OHOS
---
+++ services/src/reporter/form_record_report.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "form_record_report.h"
+#include "fms_log_wrapper.h"
+#include "form_event_report.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+FormRecordReport::FormRecordReport()
+{
+}
+
+FormRecordReport::~FormRecordReport()
+{
+    ClearReportInfo();
+}
+
+std::map<int64_t, FormRecordReportInfo>& FormRecordReport::GetFormRecords()
+{
+    return formRecordReportMap_;
+}
+
+void FormRecordReport::SetFormRecordRecordInfo(int64_t formId, const Want &want)
+{
+    FormRecordReportInfo info;
+    std::lock_guard<std::mutex> guard(formRecordReportMutex_);
+    info.bundleName = want.GetElement().GetBundleName();
+    info.moduleName = want.GetStringParam(Constants::PARAM_MODULE_NAME_KEY);
+    info.formName = want.GetStringParam(Constants::PARAM_FORM_NAME_KEY);
+    if (formRecordReportMap_.find(formId) == formRecordReportMap_.end()) {
+        formRecordReportMap_[formId] = info;
+    }
+}
+
+void FormRecordReport::IncreaseUpdateTimes(int64_t formId, HiSysEventPointType type)
+{
+    HILOG_DEBUG("formId:%{public}" PRId64, formId);
+    FormRecordReportInfo info;
+    std::lock_guard<std::mutex> guard(formRecordReportMutex_);
+    if (formRecordReportMap_.find(formId) != formRecordReportMap_.end()) {
+        info = formRecordReportMap_[formId];
+        switch (type) {
+            case TYPE_DAILY_REFRESH:
+                info.dailyRefreshTimes++;
+                break;
+            case TYPE_INVISIBLE_UPDATE:
+                info.invisibleRefreshTimes++;
+                break;
+            case TYPE_HIGH_FREQUENCY:
+                info.hfRefreshBlockTimes++;
+                break;
+            case TYPE_INVISIBLE_INTERCEPT:
+                info.invisibleRefreshBlockTimes++;
+                break;
+            case TYPE_HIGH_LOAD:
+                info.highLoadRefreshBlockTimes = 0;
+                break;
+            case TYPE_ACTIVE_RECVOER_UPDATE:
+                info.activeRecoverRefreshTimes = 0;
+                break;
+            case TYPE_PASSIVE_RECOVER_UPDATE:
+                info.passiveRecoverRefreshTimes++;
+                break;
+            case TYPE_HF_RECOVER_UPDATE:
+                info.hfRecoverRefreshTimes++;
+                break;
+            case TYPE_OFFLOAD_RECOVER_UPDATE:
+                info.offloadRecoverRefreshTimes = 0;
+                break;
+            default:
+                break;
+        }
+        formRecordReportMap_[formId] = info;
+    }
+}
+
+void FormRecordReport::HandleFormRefreshCount()
+{
+    std::lock_guard<std::mutex> guard(formRecordReportMutex_);
+    for (const auto& entry : formRecordReportMap_) {
+        int64_t formId = entry.first;
+        if (formId <= 0) {
+            return;
+        }
+        FormRecordReportInfo record = entry.second;
+        NewFormEventInfo eventInfo;
+        eventInfo.formId = formId;
+        eventInfo.bundleName = record.bundleName;
+        eventInfo.moduleName = record.moduleName;
+        eventInfo.formName = record.formName;
+        eventInfo.dailyRefreshTimes = record.dailyRefreshTimes;
+        eventInfo.invisibleRefreshTimes = record.invisibleRefreshTimes;
+        eventInfo.hfRefreshBlockTimes = record.hfRefreshBlockTimes;
+        eventInfo.invisibleRefreshBlockTimes = record.invisibleRefreshBlockTimes;
+        eventInfo.highLoadRefreshBlockTimes = record.highLoadRefreshBlockTimes;
+        eventInfo.activeRecoverRefreshTimes = record.activeRecoverRefreshTimes;
+        eventInfo.passiveRecoverRefreshTimes = record.passiveRecoverRefreshTimes;
+        eventInfo.hfRecoverRefreshTimes = record.hfRecoverRefreshTimes;
+        eventInfo.offloadRecoverRefreshTimes = record.offloadRecoverRefreshTimes;
+        FormEventReport::SendFormRefreshCountEvent(FormEventName::UPDATE_FORM_REFRESH_TIMES,
+            HiSysEventType::STATISTIC, eventInfo);
+    }
+    ClearReportInfo();
+}
+
+void FormRecordReport::ClearReportInfo()
+{
+    formRecordReportMap_.clear();
+}
+}  // namespace AppExecFwk
+}  // namespace OHOS
