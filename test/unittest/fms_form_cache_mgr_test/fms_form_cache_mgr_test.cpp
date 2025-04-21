393ed8a88d4b881631cff1f2bd2580222bc2c2c5
侯志雄 <houzhixiong1@h-partners.com>
2025-04-21 03:29:03 +0000

    update test/unittest/fms_form_cache_mgr_test/fms_form_cache_mgr_test.cpp.
    Signed-off-by: 侯志雄 <houzhixiong1@h-partners.com>

--- test/unittest/fms_form_cache_mgr_test/fms_form_cache_mgr_test.cpp
+++ test/unittest/fms_form_cache_mgr_test/fms_form_cache_mgr_test.cpp
@@ -481,36 +481,36 @@ HWTEST_F(FmsFormCacheMgrTest, FmsFormCacheMgrTest_018, TestSize.Level0)
  */
  HWTEST_F(FmsFormCacheMgrTest, FmsFormCacheMgrTest_019, TestSize.Level0)
  {
-     HILOG_INFO("FmsFormCacheMgrTest_019 start");
-     std::string result1 = "{\"a\":\"1\",\"b\":\"2\"}";
-     std::string result2 = "{\"a\":\"2\",\"b\":\"2\"}";
-     nlohmann::json dataResult1 = R"(
-         {
-             "a" : "1",
-             "b" : "2"
-         }
-     )"_json;
-     nlohmann::json dataResult2 = R"(
-         {
-             "a" : "2",
-             "b" : "2"
-         }
-     )"_json;
-     FormProviderData formProviderData;
-     formProviderData.UpdateData(dataResult1);
-     EXPECT_TRUE(formCacheMgr_.AddData(PARAM_FORM_ID_FIRST, formProviderData));
- 
-     std::string queryResult;
-     std::map<std::string, std::pair<sptr<FormAshmem>, int32_t>> imageDataMap;
-     EXPECT_TRUE(formCacheMgr_.GetData(PARAM_FORM_ID_FIRST, queryResult, imageDataMap));
-     EXPECT_EQ(result1, queryResult);
- 
-     formProviderData.UpdateData(dataResult2);
-     EXPECT_TRUE(formCacheMgr_.AddData(PARAM_FORM_ID_FIRST, formProviderData));
-     EXPECT_TRUE(formCacheMgr_.GetData(PARAM_FORM_ID_FIRST, queryResult, imageDataMap));
-     EXPECT_EQ(result2, queryResult);
-     GTEST_LOG_(INFO) << "FmsFormCacheMgrTest_019 end";
- }
+    HILOG_INFO("FmsFormCacheMgrTest_019 start");
+    std::string result1 = "{\"a\":\"1\",\"b\":\"2\"}";
+    std::string result2 = "{\"a\":\"2\",\"b\":\"2\"}";
+    nlohmann::json dataResult1 = R"(
+        {
+            "a" : "1",
+            "b" : "2"
+        }
+    )"_json;
+    nlohmann::json dataResult2 = R"(
+        {
+            "a" : "2",
+            "b" : "2"
+        }
+    )"_json;
+    FormProviderData formProviderData;
+    formProviderData.UpdateData(dataResult1);
+    EXPECT_TRUE(formCacheMgr_.AddData(PARAM_FORM_ID_FIRST, formProviderData));
+
+    std::string queryResult;
+    std::map<std::string, std::pair<sptr<FormAshmem>, int32_t>> imageDataMap;
+    EXPECT_TRUE(formCacheMgr_.GetData(PARAM_FORM_ID_FIRST, queryResult, imageDataMap));
+    EXPECT_EQ(result1, queryResult);
+
+    formProviderData.UpdateData(dataResult2);
+    EXPECT_TRUE(formCacheMgr_.AddData(PARAM_FORM_ID_FIRST, formProviderData));
+    EXPECT_TRUE(formCacheMgr_.GetData(PARAM_FORM_ID_FIRST, queryResult, imageDataMap));
+    EXPECT_EQ(result2, queryResult);
+    GTEST_LOG_(INFO) << "FmsFormCacheMgrTest_019 end";
+}
 
  /*
  * Feature: FormCacheMgr
