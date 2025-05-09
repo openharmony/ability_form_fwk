ba44b6c9134c110179e92ddd1ee4a4e86a08c44d
hutiezheng <hutiezheng@huawei.com>
2025-05-09 16:16:37 +0800

    FmsFormHostDelegateStubTest、FmsFormHostProxyTest测试套异常
    Signed-off-by: hutiezheng <hutiezheng@huawei.com>

--- test/unittest/fms_form_host_proxy_test/fms_form_host_proxy_test.cpp
+++ test/unittest/fms_form_host_proxy_test/fms_form_host_proxy_test.cpp
@@ -393,4 +393,73 @@ HWTEST_F(FmsFormHostProxyTest, OnEnableFormTest_0301, Function | MediumTest | Le
     proxy->OnEnableForm(formIds, false);
     EXPECT_NE(proxy, nullptr);
 }
+
+/*
+ * @tc.name: FormHostProxyTest_001
+ * @tc.desc: test OnError function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FmsFormHostProxyTest, FormHostProxyTest_001, Function | MediumTest | Level1)
+{
+    GTEST_LOG_(INFO)
+        << "FmsFormHostProxyTest, FormHostProxyTest_001, TestSize.Level1";
+    MockWriteInterfaceToken(false);
+    int32_t errorCode = 1;
+    std::string errorMsg = "errorMsg";
+    std::vector<int64_t> formIds{0};
+    sptr<MockIRemoteObject> iremoteObject = new (std::nothrow) MockIRemoteObject();
+    EXPECT_NE(nullptr, iremoteObject);
+    std::shared_ptr<FormHostProxy> proxy = std::make_shared<FormHostProxy>(iremoteObject);
+    EXPECT_NE(nullptr, proxy);
+    proxy->OnError(errorCode, errorMsg);
+    proxy->OnError(errorCode, errorMsg, formIds);
+    MockWriteInterfaceToken(true);
+    proxy->OnError(errorCode, errorMsg);
+    proxy->OnError(errorCode, errorMsg, formIds);
+    EXPECT_NE(proxy, nullptr);
+}
+
+/*
+ * @tc.name: FormHostProxyTest_002
+ * @tc.desc: test OnRecycleForm function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FmsFormHostProxyTest, FormHostProxyTest_002, Function | MediumTest | Level1)
+{
+    GTEST_LOG_(INFO)
+        << "FmsFormHostProxyTest, FormHostProxyTest_002, TestSize.Level1";
+    MockWriteInterfaceToken(false);
+    int64_t formId = 1;
+    sptr<MockIRemoteObject> iremoteObject = new (std::nothrow) MockIRemoteObject();
+    EXPECT_NE(nullptr, iremoteObject);
+    std::shared_ptr<FormHostProxy> proxy = std::make_shared<FormHostProxy>(iremoteObject);
+    EXPECT_NE(nullptr, proxy);
+    proxy->OnRecycleForm(formId);
+    MockWriteInterfaceToken(true);
+    proxy->OnRecycleForm(formId);
+    EXPECT_NE(proxy, nullptr);
+}
+
+/*
+ * @tc.name: FormHostProxyTest_003
+ * @tc.desc: test OnLockForm function
+ * @tc.type: FUNC
+ */
+HWTEST_F(FmsFormHostProxyTest, FormHostProxyTest_003, Function | MediumTest | Level1)
+{
+    GTEST_LOG_(INFO)
+        << "FmsFormHostProxyTest, FormHostProxyTest_003, TestSize.Level1";
+    MockWriteInterfaceToken(false);
+    std::vector<int64_t> formIds{0};
+    sptr<MockIRemoteObject> iremoteObject = new (std::nothrow) MockIRemoteObject();
+    EXPECT_NE(nullptr, iremoteObject);
+    std::shared_ptr<FormHostProxy> proxy = std::make_shared<FormHostProxy>(iremoteObject);
+    EXPECT_NE(nullptr, proxy);
+    proxy->OnLockForm(formIds, true);
+    proxy->OnLockForm(formIds, false);
+    MockWriteInterfaceToken(true);
+    proxy->OnLockForm(formIds, true);
+    proxy->OnLockForm(formIds, false);
+    EXPECT_NE(proxy, nullptr);
+}
 }
