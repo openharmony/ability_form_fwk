b9bb04dc6697b04a435757f6b4e2c8ba602aaf31
xiangcong <xiangcong4@huawei.com>
2025-08-19 09:56:56 +0800

    修改检视意见
    Signed-off-by: xiangcong <xiangcong4@huawei.com>

--- interfaces/inner_api/include/form_file_util.h
+++ interfaces/inner_api/include/form_file_util.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -25,7 +25,6 @@ public:
     static void GetDirFiles(const std::string &path, std::vector<std::string> &files);
     static void GetFilesSize(std::vector<std::string> &files, std::vector<std::uint64_t> &filesSize);
 
-private:
 };
 } // namespace AppExecFwk
 } // namespace OHOS
