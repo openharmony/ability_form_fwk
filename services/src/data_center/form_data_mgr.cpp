0dafc66668eed8d766c79f18d92453e2e02a38d5
hutz_hw <hutiezheng@huawei.com>
2025-09-23 15:39:00 +0800

    update form_data_mgr
    Signed-off-by: hutz_hw <hutiezheng@huawei.com>

--- services/src/data_center/form_data_mgr.cpp
+++ services/src/data_center/form_data_mgr.cpp
@@ -378,6 +378,21 @@ int FormDataMgr::CheckTempEnoughForm() const
     return ERR_OK;
 }
 
+std::map<Constants::FormLocation, int> GetLocationMap()
+{
+    std::vector<FormDBInfo> formDBInfos;
+    FormDbCache::GetInstance().GetAllFormInfo(formDBInfos);
+    std::map<Constants::FormLocation, int> locationMap;
+    for (const auto& dbInfo : formDBInfos) {
+        if (locationMap.count(dbInfo.formLocation) == 0) {
+            locationMap[dbInfo.formLocation] = 1;
+        } else {
+            locationMap[dbInfo.formLocation] = locationMap[dbInfo.formLocation] + 1;
+        }
+    }
+    return locationMap;
+}
+
 /**
  * @brief Check form count is max.
  * @param callingUid The UID of the proxy.
@@ -408,16 +423,7 @@ int FormDataMgr::CheckEnoughForm(const int callingUid, const int32_t currentUser
     const auto formDbInfoSize = FormDbCache::GetInstance().GetAllFormInfoSize();
     HILOG_INFO("already use %{public}d forms by userId", formDbInfoSize);
     if (formDbInfoSize >= maxFormsSize) {
-        std::vector<FormDBInfo> formDBInfos;
-        FormDbCache::GetInstance().GetAllFormInfo(formDBInfos);
-        std::map<Constants::FormLocation, int> locationMap;
-        for (const auto& dbInfo : formDBInfos) {
-            if (locationMap.count(dbInfo.formLocation) == 0) {
-                locationMap[dbInfo.formLocation] = 1;
-            } else {
-                locationMap[dbInfo.formLocation] = locationMap[dbInfo.formLocation] + 1;
-            }
-        }
+        std::map<Constants::FormLocation, int> locationMap = GetLocationMap();
         Constants::FormLocation maxLocation = Constants::FormLocation::OTHER;
         int maxCount = 0;
         for (auto &location : locationMap) {
