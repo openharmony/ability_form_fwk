d9b22a5cb8b390148e9bd26a43661ddd149cd30f
zhangsaiyang1 <zhangsaiyang1@h-partners.com>
2026-02-03 15:11:51 +0800

    修改检视意见
    Signed-off-by: zhangsaiyang1 <zhangsaiyang1@h-partners.com>

--- services/src/form_render/form_render_mgr_inner.cpp
+++ services/src/form_render/form_render_mgr_inner.cpp
@@ -89,8 +89,8 @@ ErrCode FormRenderMgrInner::RenderForm(
         return RenderConnectedForm(formRecord, want, connection);
     }
 
-    sptr<IAbilityConnection> formRenderConnection = new (std::nothrow) FormRenderConnection(formRecord,
-        want.GetParams());
+    sptr<FormRenderConnection> formRenderConnection = new (std::nothrow) FormRenderConnection(
+        formRecord, want.GetParams());
     if (formRenderConnection == nullptr) {
         HILOG_ERROR("null formRenderConnection");
         return ERR_APPEXECFWK_FORM_BIND_PROVIDER_FAILED;
