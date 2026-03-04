ddda6e0e2287da857a862dbd4501b4ab95ea9e7d
zhangsaiyang1 <zhangsaiyang1@h-partners.com>
2026-03-04 14:48:10 +0800

    修改检视意见
    Signed-off-by: zhangsaiyang1 <zhangsaiyang1@h-partners.com>

--- test/fuzztest/formiteminfo_fuzzer/formiteminfo_fuzzer.cpp
+++ test/fuzztest/formiteminfo_fuzzer/formiteminfo_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,117 +17,235 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
 
 #define private public
 #define protected public
 #include "data_center/form_info/form_item_info.h"
 #undef private
 #undef protected
-#include "securec.h"
 
 using namespace OHOS::AppExecFwk;
 
 namespace OHOS {
-constexpr size_t U32_AT_SIZE = 4;
-constexpr uint8_t ENABLE = 2;
-uint32_t GetU32Data(const char* ptr)
+constexpr int32_t MAX_LENGTH = 256;
+
+bool DoSomethingInterestingWithMyAPI(FuzzedDataProvider *fdp)
 {
-    // convert fuzz input data to an integer
-    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
-}
-bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
-{
+    if (fdp == nullptr) {
+        return true;
+    }
+
     FormItemInfo formItemInfo;
-    std::string dir(data, size);
+
+    std::string dir(fdp->ConsumeRandomLengthString(MAX_LENGTH));
     std::vector<std::string> dirs;
     dirs.emplace_back(dir);
     formItemInfo.SetHapSourceDirs(dirs);
     formItemInfo.GetHapSourceDirs(dirs);
-    formItemInfo.IsTemporaryForm();
-    std::string moduleName(data, size);
+
+    std::string moduleName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
     formItemInfo.GetHapSourceByModuleName(moduleName);
-    formItemInfo.IsValidItem();
-    FormRecord record;
-    formItemInfo.IsMatch(record);
-    formItemInfo.IsSameFormConfig(record);
-    formItemInfo.IsFormVisibleNotify();
-    std::string left(data, size);
-    std::string right(data, size);
-    formItemInfo.IsEqual(left, right);
-    int64_t formId = static_cast<int64_t>(GetU32Data(data));
+
+    std::string hapSourceDir(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.AddModuleInfo(moduleName, hapSourceDir);
+
+    std::string packageName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.AddModulePkgName(moduleName, packageName);
+
+    int64_t formId = fdp->ConsumeIntegral<int64_t>();
     formItemInfo.SetFormId(formId);
-    std::string packageName(data, size);
-    formItemInfo.SetPackageName(packageName);
-    std::string providerBundleName(data, size);
-    formItemInfo.SetProviderBundleName(providerBundleName);
-    std::string hostBundleName(data, size);
-    formItemInfo.SetHostBundleName(hostBundleName);
-    formItemInfo.SetModuleName(moduleName);
-    std::string abilityName(data, size);
-    formItemInfo.SetAbilityName(abilityName);
-    std::string formName(data, size);
-    formItemInfo.SetFormName(formName);
-    std::string jsComponentName(data, size);
-    formItemInfo.SetJsComponentName(jsComponentName);
-    std::string abilityModuleName(data, size);
-    formItemInfo.SetAbilityModuleName(abilityModuleName);
-    int specificationId = static_cast<int>(GetU32Data(data));
-    formItemInfo.SetSpecificationId(specificationId);
-    bool isEnableUpdateFlag = *data % ENABLE;
-    formItemInfo.SetEnableUpdateFlag(isEnableUpdateFlag);
-    int updateDuration = static_cast<int>(GetU32Data(data));
-    formItemInfo.SetUpdateDuration(updateDuration);
-    std::string scheduledUpdateTime(data, size);
-    formItemInfo.SetScheduledUpdateTime(scheduledUpdateTime);
-    std::string hapSourceDir(data, size);
-    formItemInfo.AddHapSourceDirs(hapSourceDir);
     formItemInfo.GetFormId();
+
+    formItemInfo.SetPackageName(packageName);
     formItemInfo.GetPackageName();
+
+    std::string providerBundleName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetProviderBundleName(providerBundleName);
     formItemInfo.GetProviderBundleName();
+
+    std::string hostBundleName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetHostBundleName(hostBundleName);
     formItemInfo.GetHostBundleName();
+
+    formItemInfo.SetModuleName(moduleName);
     formItemInfo.GetModuleName();
+
+    std::string abilityName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetAbilityName(abilityName);
     formItemInfo.GetAbilityName();
+
+    std::string formName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetFormName(formName);
     formItemInfo.GetFormName();
+
+    std::string jsComponentName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetJsComponentName(jsComponentName);
     formItemInfo.GetJsComponentName();
+
+    std::string abilityModuleName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetAbilityModuleName(abilityModuleName);
     formItemInfo.GetAbilityModuleName();
+
+    int specificationId = fdp->ConsumeIntegral<int>();
+    formItemInfo.SetSpecificationId(specificationId);
     formItemInfo.GetSpecificationId();
+
+    bool isEnableUpdateFlag = fdp->ConsumeBool();
+    formItemInfo.SetEnableUpdateFlag(isEnableUpdateFlag);
+    formItemInfo.IsEnableUpdateFlag();
+
+    int updateDuration = fdp->ConsumeIntegral<int>();
+    formItemInfo.SetUpdateDuration(updateDuration);
     formItemInfo.GetUpdateDuration();
+
+    std::string scheduledUpdateTime(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetScheduledUpdateTime(scheduledUpdateTime);
     formItemInfo.GetScheduledUpdateTime();
-    bool temporaryFlag = *data % ENABLE;
+
+    std::string multiScheduledUpdateTime(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetMultiScheduledUpdateTime(multiScheduledUpdateTime);
+    formItemInfo.GetMultiScheduledUpdateTime();
+
+    bool temporaryFlag = fdp->ConsumeBool();
     formItemInfo.SetTemporaryFlag(temporaryFlag);
-    std::string moduleSourceDir(data, size);
-    formItemInfo.AddModuleInfo(moduleName, moduleSourceDir);
-    return formItemInfo.IsEnableUpdateFlag();
+    formItemInfo.IsTemporaryForm();
+
+    bool isFormVisibleNotify = fdp->ConsumeBool();
+    formItemInfo.SetFormVisibleNotify(isFormVisibleNotify);
+    formItemInfo.IsFormVisibleNotify();
+
+    std::string formSrc(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetFormSrc(formSrc);
+    formItemInfo.GetFormSrc();
+
+    FormWindow formWindow;
+    formWindow.designWidth = fdp->ConsumeIntegral<int32_t>();
+    formItemInfo.SetFormWindow(formWindow);
+    formItemInfo.GetFormWindow();
+
+    uint32_t versionCode = fdp->ConsumeIntegral<uint32_t>();
+    formItemInfo.SetVersionCode(versionCode);
+    formItemInfo.GetVersionCode();
+
+    std::string versionName(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetVersionName(versionName);
+    formItemInfo.GetVersionName();
+
+    uint32_t compatibleVersion = fdp->ConsumeIntegral<uint32_t>();
+    formItemInfo.SetCompatibleVersion(compatibleVersion);
+    formItemInfo.GetCompatibleVersion();
+
+    formItemInfo.GetIcon();
+
+    std::string deviceId(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetDeviceId(deviceId);
+    formItemInfo.GetDeviceId();
+
+    FormType type = FormType::ETS;
+    formItemInfo.SetType(type);
+    formItemInfo.GetType();
+
+    bool isDynamic = fdp->ConsumeBool();
+    formItemInfo.SetIsDynamic(isDynamic);
+    formItemInfo.IsDynamic();
+
+    bool transparencyEnabled = fdp->ConsumeBool();
+    formItemInfo.SetTransparencyEnabled(transparencyEnabled);
+    formItemInfo.IsTransparencyEnabled();
+
+    int32_t privacyLevel = fdp->ConsumeIntegral<int32_t>();
+    formItemInfo.SetPrivacyLevel(privacyLevel);
+    formItemInfo.GetPrivacyLevel();
+
+    FormType uiSyntax = FormType::ETS;
+    formItemInfo.SetUiSyntax(uiSyntax);
+    formItemInfo.GetUiSyntax();
+
+    bool isSystemApp = fdp->ConsumeBool();
+    formItemInfo.SetSystemAppFlag(isSystemApp);
+    formItemInfo.GetSystemAppFlag();
+
+    bool dataProxyEnabled = fdp->ConsumeBool();
+    formItemInfo.SetDataProxyFlag(dataProxyEnabled);
+    formItemInfo.GetDataProxyFlag();
+
+    int32_t uid = fdp->ConsumeIntegral<int32_t>();
+    formItemInfo.SetProviderUid(uid);
+    formItemInfo.GetProviderUid();
+
+    std::string description(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetDescription(description);
+    formItemInfo.GetDescription();
+
+    Constants::FormLocation formLocation = Constants::FormLocation::DESKTOP;
+    formItemInfo.SetFormLocation(formLocation);
+    formItemInfo.GetFormLocation();
+
+    bool isThemeForm = fdp->ConsumeBool();
+    formItemInfo.SetIsThemeForm(isThemeForm);
+    formItemInfo.GetIsThemeForm();
+    BundleType bundleType = BundleType::APP_SERVICE_FWK;
+    formItemInfo.SetFormBundleType(bundleType);
+    formItemInfo.GetFormBundleType();
+
+    bool enableForm = fdp->ConsumeBool();
+    formItemInfo.SetEnableForm(enableForm);
+    formItemInfo.IsEnableForm();
+
+    Constants::RenderingMode renderingMode = Constants::RenderingMode::FULL_COLOR;
+    formItemInfo.SetRenderingMode(renderingMode);
+    formItemInfo.GetRenderingMode();
+
+    bool lockForm = fdp->ConsumeBool();
+    formItemInfo.SetLockForm(lockForm);
+    formItemInfo.IsLockForm();
+
+    bool protectForm = fdp->ConsumeBool();
+    formItemInfo.SetProtectForm(protectForm);
+    formItemInfo.IsProtectForm();
+
+    std::vector<int32_t> conditionUpdate;
+    int32_t numConditions = fdp->ConsumeIntegralInRange(0, 10);
+    for (int32_t i = 0; i < numConditions; i++) {
+        conditionUpdate.push_back(fdp->ConsumeIntegral<int32_t>());
+    }
+    formItemInfo.SetConditionUpdate(conditionUpdate);
+    formItemInfo.GetConditionUpdate();
+
+    bool dataProxyIgnoreFormVisibility = fdp->ConsumeBool();
+    formItemInfo.SetDataProxyIgnoreFormVisibility(dataProxyIgnoreFormVisibility);
+    formItemInfo.GetDataProxyIgnoreFormVisibility();
+
+    bool isDistributedForm = fdp->ConsumeBool();
+    formItemInfo.SetDistributedForm(isDistributedForm);
+    formItemInfo.IsDistributedForm();
+
+    std::string uiModule(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetUiModuleName(uiModule);
+    formItemInfo.GetUiModuleName();
+
+    std::string templateFormImperativeFwk(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.SetTemplateFormImperativeFwk(templateFormImperativeFwk);
+    formItemInfo.GetTemplateFormImperativeFwk();
+
+    bool isTemplateForm = fdp->ConsumeBool();
+    formItemInfo.SetIsTemplateForm(isTemplateForm);
+    formItemInfo.GetIsTemplateForm();
+
+    formItemInfo.IsValidItem();
+
+    std::string left(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    std::string right(fdp->ConsumeRandomLengthString(MAX_LENGTH));
+    formItemInfo.IsEqual(left, right);
+
+    return true;
 }
 }
 
-/* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
-    /* Run your code on data */
-    if (data == nullptr) {
-        return 0;
-    }
-
-    if (size < OHOS::U32_AT_SIZE) {
-        return 0;
-    }
-
-    char* ch = static_cast<char*>(malloc(size + 1));
-    if (ch == nullptr) {
-        return 0;
-    }
-
-    (void)memset_s(ch, size + 1, 0x00, size + 1);
-    if (memcpy_s(ch, size + 1, data, size) != EOK) {
-        free(ch);
-        ch = nullptr;
-        return 0;
-    }
-
-    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
-    free(ch);
-    ch = nullptr;
+    FuzzedDataProvider fdp(data, size);
+    OHOS::DoSomethingInterestingWithMyAPI(&fdp);
     return 0;
 }
-
