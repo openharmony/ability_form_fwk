abb24cb7b53e1582a245b91bc9f0587285911359
allenlee6 <lishengyang5@huawei.com>
2026-07-21 23:19:04 +0800

    update: 更新文件 form_data_adapter.h
    Signed-off-by: allenlee6 <lishengyang5@huawei.com>

--- services/include/form_mgr/form_data_adapter.h
+++ services/include/form_mgr/form_data_adapter.h
@@ -110,7 +110,8 @@ public:
 private:
     ErrCode InnerAcquireProviderFormInfoAsync(const int64_t formId,
         const FormItemInfo &info, const WantParams &wantParams);
-
+    
+    ErrCode ValidateAndGetFormRecord(const int64_t formId, int64_t &matchedFormId, FormRecord &formRecord);
     ErrCode HandleUpdateFormFlag(const std::vector<int64_t> &formIds,
         const sptr<IRemoteObject> &callerToken, bool flag, bool isOnlyEnableUpdate);
     void UpdateFormRenderParamsAfterReload(const int64_t formId);
