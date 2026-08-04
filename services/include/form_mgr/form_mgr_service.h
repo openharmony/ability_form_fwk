2d6ed91843dc28a6bf1ed357fe0ad8657cd0ea37
zhangsaiyang1 <zhangsaiyang1@h-partners.com>
2026-08-04 19:14:28 +0800

    onKvDataServiceAddTime_需要锁保护 Co-Authored-By: Agent
    Signed-off-by: zhangsaiyang1 <zhangsaiyang1@h-partners.com>

--- services/include/form_mgr/form_mgr_service.h
+++ services/include/form_mgr/form_mgr_service.h
@@ -1073,6 +1073,7 @@ private:
     std::string onStartPublishTime_;
     std::string onStartEndTime_;
     std::string onKvDataServiceAddTime_;
+    std::mutex onKvDataServiceAddTimeMutex_;
     ServiceRunningState state_ = ServiceRunningState::STATE_NOT_START;
     std::shared_ptr<FormEventHandler> handler_ = nullptr;
     std::shared_ptr<FormSerialQueue> serialQueue_ = nullptr;
