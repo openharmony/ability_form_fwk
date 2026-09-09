e128d9729e3863052d9c9fe2fed5e94193d3a6ea
zhangsaiyang1 <zhangsaiyang1@h-partners.com>
2026-09-09 17:08:42 +0800

    回退fuzz sleep Co-Authored-By: codeagent Signed-off-by: zhangsaiyang1 <zhangsaiyang1@h-partners.com>

--- test/fuzztest/forminfohelper_fuzzer/forminfohelper_fuzzer.cpp
+++ test/fuzztest/forminfohelper_fuzzer/forminfohelper_fuzzer.cpp
@@ -116,7 +116,6 @@ bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
 
 extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
     return 0;
 }
 
